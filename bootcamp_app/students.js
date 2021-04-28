385814c1c601d1ac81b7d5a1eeedebc8d49a4e29
Grigor Baltadjian <gb795@hotmail.com>
2021-04-28 21:18:24 +0000

    SQL injection security features are included

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 let input = process.argv.slice(2);
 let cohortName = input[0];
 let limit = input[1];
+const values = [`%${cohortName}%`, `${limit}` ];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -11,14 +12,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT students.id, students.name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortName}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
+
 .then(res => {
   console.log(res.rows);
 })
-.catch(err => console.error('query error', err.stack));
+.catch(err => console.error('query error', err.stack)); 
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 
 let input = process.argv.slice(2);
 let cohortName = input[0];
+const values = [`%${cohortName}%`];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -10,16 +11,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohortName}$'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
 })
